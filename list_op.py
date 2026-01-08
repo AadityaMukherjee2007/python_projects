96ac094342326dd57dabc4610cd429c9ef1c5bad
Aaditya Mukherjee <84586322+AadityaMukherjee2007@users.noreply.github.com>
2026-01-08 07:15:09 +0000

    2nd class

---
+++ list_op.py
@@ -0,0 +1,12 @@
+l_2d = []
+
+# for _ in range(4):
+#     l_1d = []
+#     for i in range(5):
+#         l_1d.append(input(f"Enter element {i} of Row{_}:"))
+#     l_2d.append(l_1d)
+
+l_2d = [['1', '2', '3', '4', '5'], ['6', '7', '8', '9', '10'], ['11', '12', '13', '14', '15'], ['16', '', '17', '18', '19']]
+print(l_2d)
+print(l_2d[1:3])
+    
