b5ef02f5df273a1ed1e37e5332ca2e8d47f804ea
Aaditya Mukherjee <84586322+AadityaMukherjee2007@users.noreply.github.com>
2026-01-13 09:29:46 +0000

    3rd class

--- flask_ui/smtp_test.py
+++ flask_ui/smtp_test.py
@@ -1,14 +1,20 @@
-from json import load
 from dotenv import load_dotenv
 import os, smtplib
 
 load_dotenv()
+cred = os.getenv("PASS", "default_pass")
+print(cred)
 
 from_add = "aaditya.mukherjee2007@gmail.com"
 to_add = "aaditya.mukherjee2007@gmail.com"
 msg = "This is a test message"
 
-server = smtplib.SMTP("localhost")
-server.set_debuglevel(1)
-server.sendmail(from_add, to_add, msg)
-server.quit()
+try:
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(from_add, cred)
+    server.sendmail(from_add, to_add, msg)
+except Exception as e:
+    print(f"An error occurred: {e}")
+finally:
+    server.quit()
